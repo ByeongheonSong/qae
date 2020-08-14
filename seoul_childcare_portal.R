c7238051ee1a8922b9254e8d09ba00d3c60041b1
jaesungc <jaesungc.econ@gmail.com>
2020-08-14 22:31:25 +0900

    Update seoul_childcare_portal.R

--- seoul_childcare_portal.R
+++ seoul_childcare_portal.R
@@ -1,5 +1,7 @@
 #1. 서울보육포털: 인코딩 이슈가 있어서 Windows 환경과 RStudio Cloud 환경에서 동작하는 코드를 구분해서 수록했습니다.
 
+## Windows 환경: 에서는 인코딩 이슈가 있어서 local 설정에 관한 부분을 추가해야 합니다.
+
 ## Clear memory
 rm(list=ls())
 
@@ -11,42 +13,56 @@ library(writexl)
 
 url <- "https://iseoul.seoul.go.kr/portal/info/preSchoolList.do?pageIndex=3"
 
-## Windows 환경: 에서는 인코딩 이슈가 있어서 local 설정에 관한 부분을 추가해야 합니다.
-
 
 ## Method 1. XML library
-txt <- readLines(url, encoding="UTF-8")
-txt_p <- htmlParse(txt, encoding="UTF-8")
+txt <- readLines(url)
+txt_p <- htmlParse(txt)
 Sys.setlocale("LC_ALL", "C")
 tab <- readHTMLTable(txt_p)
-Sys.setlocale("LC_ALL", "Korean_Korea.949")
-df<-tab[[1]]
-head(df)
-write_xlsx(df, "seoul_childcare_center_p3.xlsx") 
+Sys.setlocale("LC_ALL", "Korean")
+df1 <- tab[[1]]
+head(df1)
+write_xlsx(df, "seoul_childcare_center_p3_XML.xlsx") 
 
 
 ## Method 2. rvest library
 Sys.setlocale("LC_ALL", "C")
-df <- read_html(url) %>% 
+df2 <- read_html(url) %>% 
   html_table() %>% 
   as.data.frame()
-Sys.setlocale("LC_ALL", "Korean_Korea.949")
-head(df)
+Sys.setlocale("LC_ALL", "Korean")
+head(df2)
 
-write_xlsx(df, "seoul_childcare_center_p3.xlsx") 
+write_xlsx(df, "seoul_childcare_center_p3_rvest.xlsx") 
+
+
+
+
 
 
 
 ## RStudio Cloud 환경(Unix 서버)
 
+## Clear memory
+rm(list=ls())
+
+library(XML)
+library(rvest)
+library(dplyr)
+library(readxl)
+library(writexl)
+
+url <- "https://iseoul.seoul.go.kr/portal/info/preSchoolList.do?pageIndex=3"
+
+
 ### Method 1. XML library
 txt <- readLines(url)
 txt_p <- htmlParse(txt)
 tab <- readHTMLTable(txt_p)
-df<-tab[[1]]
+df <- tab[[1]]
 head(df)
 
-write_xlsx(df, "seoul_childcare_center_p3.xlsx") 
+write_xlsx(df, "seoul_childcare_center_p3_XML.xlsx") 
 
 
 ### Method 2. rvest library
@@ -55,4 +71,4 @@ df <- read_html(url) %>%
   as.data.frame()
 head(df)
 
-write_xlsx(df, "seoul_childcare_center_p3.xlsx") 
+write_xlsx(df, "seoul_childcare_center_p3_rvest.xlsx") 
