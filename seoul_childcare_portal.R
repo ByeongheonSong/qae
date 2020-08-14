9428a5724d240ed4c40da0761fd3dd21fbccc675
jaesungc <jaesungc.econ@gmail.com>
2020-08-14 15:24:43 +0900

    Update seoul_childcare_portal.R

--- seoul_childcare_portal.R
+++ seoul_childcare_portal.R
@@ -5,18 +5,20 @@ rm(list=ls())
 
 library(XML)
 library(rvest)
-library(xlsx)
 library(dplyr)
+library(readxl)
+library(writexl)
 
 url <- "https://iseoul.seoul.go.kr/portal/info/preSchoolList.do?pageIndex=3"
+
 
 ## Method 1. XML library
 txt <- readLines(url)
 txt_p <- htmlParse(txt)
 tab <- readHTMLTable(txt_p)
-df <- tab[[1]]
+df<-tab[[1]]
 
-write.xlsx(df, "seoul_childcare_center_p3.xlsx") 
+write_xlsx(df, "seoul_childcare_center_p3.xlsx") 
 
 
 ## Method 2. rvest library
@@ -24,4 +26,4 @@ df <- read_html(url) %>%
   html_table() %>% 
   as.data.frame()
 
-write.xlsx(df, "seoul_childcare_center_p3.xlsx")
+write_xlsx(df, "seoul_childcare_center_p3.xlsx") 
