c8b3002c489e1d33795a938fd3e245e28a020dd3
jaesungc <jaesungc.econ@gmail.com>
2020-08-18 00:41:37 +0900

    Create tedtalk_rvest.R

---
+++ tedtalk_rvest.R
@@ -0,0 +1,91 @@
+# Ted Talks
+
+library(rvest)
+library(XML)
+library(dplyr)
+library(stringr)
+library(writexl)
+
+
+
+## Case 1. Extract names of speakers from the first page.
+
+## rvest method
+baseURL <- "http://www.ted.com/talks?page=1"
+res <- read_html(baseURL)
+pattern <- "//div[@class='container results']//h4[@class='h12 talk-link__speaker']"
+res %>% html_nodes(xpath=pattern) %>% 
+  html_text()
+
+
+
+
+
+## Case 2. We will scrap first three pages from the website.
+
+n.page <- 10
+
+## Step 1. Download Source files for each page
+for (i in 1:n.page) {
+  
+  if (i%%5==1) print(str_c("Downloading source file for page ", i, " is in progress!"))
+  baseURL <- str_c("http://www.ted.com/talks?page=", i)
+  txt <- readLines(baseURL, warn=FALSE)
+  outfile <- str_c("tedtalk", i, ".html")
+  writeLines(txt, outfile)
+  
+  Sys.sleep(1)
+  
+}
+
+
+
+## Step 2. Extracting information from each page
+
+## rvest way
+Stack<-NULL
+for (i in 1:n.page) {
+  
+  if (i%%5==1) print(str_c("Scraping page ", i, " is starting"))
+  
+  infile <- str_c("tedtalk", i, ".html")
+  res <- read_html(infile)
+  
+  pattern <- "//div[@class='container results']//h4[@class='h12 talk-link__speaker']"
+  e1 <- res %>% 
+    html_nodes(xpath=pattern) %>% 
+    html_text()
+  
+  pattern <- "//div[@class='container results']//h4//a"
+  e2 <- res %>% 
+    html_nodes(xpath=pattern) %>% 
+    html_text() %>%
+    str_trim()
+  
+  pattern <- "//div[@class='meta']//span[@class='meta__item']//span[@class='meta__val']"
+  e3 <- res %>% 
+    html_nodes(xpath=pattern) %>% 
+    html_text() %>% 
+    str_trim()
+  
+  pattern <- "//div[@class='container results']//h4/a[@class=' ga-link']"
+  e4 <- res %>% 
+    html_nodes(xpath=pattern) %>%
+    html_attr("href") %>%
+    str_c("http://www.ted.com", .)
+  
+  tab <- data.frame(cbind(e1, e2, e3, e4))
+  
+  Stack <- rbind(Stack, tab)
+  
+}
+
+colnames(Stack) <- c("speaker", "title", "posted", "link")
+head(Stack)
+
+outfile <- "tedtalk_list.xlsx"
+write_xlsx(Stack, outfile)
+
+
+
+
